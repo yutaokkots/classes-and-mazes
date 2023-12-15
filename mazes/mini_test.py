ba766f327217200e3cf36c60f4d38ca0a411826d
yutaokkots <yuta.okkotsu@gmail.com>
2023-12-14 22:35:52 -0700

    Updated the maze class, created a small version of the test maze.

---
+++ mazes/mini_test.py
@@ -0,0 +1,23 @@
+""" """
+
+from maze_solver.models.border import Border
+from maze_solver.models.maze import Maze
+from maze_solver.models.role import Role
+from maze_solver.models.square import Square
+
+mini_maze = Maze(
+    square=(
+        Square(0, 0, 0, Border.TOP | Border.LEFT),
+        Square(1, 0, 1, Border.TOP | Border.RIGHT),
+        Square(2, 0, 2, Border.LEFT | Border.RIGHT, Role.EXIT),
+        Square(3, 0, 3, Border.TOP | Border.LEFT | Border.RIGHT),
+        Square(4, 1, 0, Border.BOTTOM | Border.LEFT | Border.RIGHT),
+        Square(5, 1, 1, Border.LEFT | Border.RIGHT),
+        Square(6, 1, 2, Border.BOTTOM | Border.LEFT),
+        Square(7, 1, 3, Border.RIGHT),
+        Square(8, 2, 0, Border.TOP | Border.LEFT | Role.ENTRANCE),
+        Square(9, 2, 1, Border.BOTTOM),
+        Square(10, 2, 2, Border.TOP | Border.BOTTOM),
+        Square(11, 2, 3, Border.BOTTOM | Border.RIGHT),
+    )
+)
--- src/maze_solver/models/maze.py
+++ src/maze_solver/models/maze.py
@@ -4,6 +4,8 @@ from dataclasses import dataclass
 from typing import Iterator
 from functools import cached_property
 from maze_solver.models.square import Square
+from maze_solver.models.maze import Maze
+from maze_solver.models.role import Role
 
 @dataclass(frozen=True) # immutable dataclass to ensure underlying tuple of square objects are unchanged.
 class Maze:
@@ -21,6 +23,13 @@ class Maze:
        __getitem__()
     """
     squares: tuple[Square, ...]
+
+
+    def __post_init__(self) -> None:
+        validate_indices(self)
+        validate_row_columns(self)
+        validate_entrance(self)
+        validate_exit(self)
 
     def __iter__(self) -> Iterator[Square]:
         """Allows maze instances to cooperate with a for-loop."""
@@ -48,4 +57,43 @@ class Maze:
         This allows for data consistency, where supplied values may not be correct.
         """
         return max(square.row for square in self) + 1
+
+    @cached_property
+    def entrance(self) -> Square:
+        return next(sq for sq in self if sq.role is Role.ENTRANCE)
+    @cached_property
+    def exit(self) -> Square:
+        return next(sq for sq in self if sq.role is Role.EXIT)
+
+
+def validate_indices(self, maze:Maze) -> None:
+    """Validates each square in the maze.
     
+    Checkes for the index property of each Square fits into 
+    a continuous sequence of numbers that enumrates 
+    all of the squares in the maze.
+    """
+    assert [square.index for square in maze] == list(range(len(maze.squares))), "Incorrect square.index."
+
+def validate_row_columns(self, maze:Maze) -> None:
+    """ Validates the row and column of the maze.
+
+    Iterates over the rows and columns in the Maze,
+    ensuring that the row and column attributes of the corresponding Square
+    match up with the current row and column of the loops.
+    """
+    for y in range(maze.height):
+        for x in range(maze.width):
+            square = maze[y * maze.width + x]
+            assert square.row == y, "Incorrect square.row."
+            assert square.column == x, "Incorrect square.column."
+
+def validate_entrance(maze: Maze) -> None:
+    assert 1 == sum(
+        1 for square in maze if square.role is Role.ENTRANCE
+    ), "Must include exactly one entrance."
+
+def validate_exit(maze: Maze) -> None:
+    assert 1 == sum(
+        1 for square in maze if square.role is Role.EXIT
+    ), "Must include exactly one exit."
